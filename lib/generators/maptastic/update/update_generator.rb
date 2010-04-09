c45daf09e7bce46e558d23a42f76fda35e585732
benedikt <benedikt@synatic.net>
2010-04-09 16:48:17 +0200

    New update generator to easily update the javascript files.

--- lib/generators/maptastic/update/update_generator.rb
+++ lib/generators/maptastic/update/update_generator.rb
@@ -1,17 +1,14 @@
 module Maptastic
   module Generators
-    class InstallGenerator < Rails::Generators::Base
-      desc "Creates a Maptastic initializer and copies the required javascript files."
+    class UpdateGenerator < Rails::Generators::Base
+      desc "Updates the required javascript files."
 
       def self.source_root
         File.expand_path("../../templates", __FILE__)
       end
-
-      def copy_initializer
-        template "maptastic.rb", "config/initializers/maptastic.rb"
-      end
-
+      
       def copy_javascripts
+        remove_file "public/javascripts/maptastic.js"
         copy_file "maptastic.js", "public/javascripts/maptastic.js"    
       end
 
